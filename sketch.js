4a2214d81f72f67a4e2287a3ad76b713620268f2
haritarun587 <79700805+haritarun587@users.noreply.github.com>
2021-04-19 11:28:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world,dustbin,paper;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+    rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+    Engine.run(engine);
+
+    dustbin = new Dustbin(720,390,100,10);
+	paper = new paper(100,300,10);
+	ground = Bodies.rectangle(width /2,400,width,10,
+	{
+		isStatic:true
+    });
+	world.add(World,ground);	
+   }
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+  background(0);
+  dustbin.display();
+  paper.display();
+ 
+}
+
+
+function Keypressed(){
+	if (keyCode  === UP_ARROW){
+		Matter.Body.applyForce(paper.Body,paper.Body.position,{
+			x:12,
+			y:-13
+		});
+	}
+}
